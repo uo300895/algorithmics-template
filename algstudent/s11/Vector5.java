afca74e5292edc50d75069610f5a66e3ca75cddc
UO300895 <UO300895runiovi.es>
2025-02-13 09:36:02 +0100

    lab 2

---
+++ algstudent/s11/Vector5.java
@@ -0,0 +1,25 @@
+package algstudent.s11;
+
+public class Vector5 {
+
+    static int[]v;
+        
+        public static void main(String arg []) {
+            int repetitions = Integer.parseInt(arg[0]);
+            long t1,t2;
+            
+            for (int n=80000; n<=Integer.MAX_VALUE; n*=5){ //n is increased *5   
+                  v = new int[n];
+                  Vector1.fillIn(v);
+                  
+                  t1 = System.currentTimeMillis();
+                  //We have to repeat the whole process to be measured
+                  for (int repetition=1; repetition<=repetitions; repetition++){    	
+                     Vector1.maximum(v, v);
+                  }
+                  t2 = System.currentTimeMillis();
+                  System.out.printf("SIZE=%d TIME=%d milliseconds NTIMES=%d\n", n, t2-t1, repetitions);	
+            }//for 
+            
+        }//main
+    }
---
+++ algstudent/s11/ws/algstudent.s11/src/algstudent/s11/Vector6.java
@@ -0,0 +1,26 @@
+package algstudent.s11;
+
+public class Vector6 {
+
+static int[]v;
+	
+	public static void main(String arg []) {
+		int repetitions = Integer.parseInt(arg[0]);
+		long t1,t2;
+		int nmatches = 0;
+		
+		for (int n=160000; n<=Integer.MAX_VALUE; n*=5){ //n is increased *5   
+			  v = new int[n];
+			  Vector1.fillIn(v);
+			  
+			  t1 = System.currentTimeMillis();
+			  //We have to repeat the whole process to be measured
+			  for (int repetition=1; repetition<=repetitions; repetition++){    	
+			     nmatches = Vector1.matches1(v,v);
+			  }
+			  t2 = System.currentTimeMillis();
+			  System.out.printf("SIZE=%d TIME=%d milliseconds number of matches=%d NTIMES=%d\n", n, t2-t1, nmatches, repetitions);	
+		}//for 
+		
+	}//main
+}
